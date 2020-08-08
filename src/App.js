327ae206fe2fd68e4f035be1b8531d92a163e536
Jon Jackson <jon.jackson@perficient.com>
2020-08-08 13:30:26 -0400

    brushed up css file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import MarkdownExample from './components/MarkdownExample';
 import SearchableDropdown from './components/SearchableDropdown';
 import HomepageLayout from './components/HomepageLayout';
 import NavBar from './components/NavBar';
