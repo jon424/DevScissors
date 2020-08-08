1b26084f86573cf94b26d56d3cb59a91bd06c31c
Jon Jackson <jon.jackson@perficient.com>
2020-08-07 22:49:34 -0400

    made new route for post and added metaPost classes for Card UI elements

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import MarkdownExample from './components/MarkdownExample';
 import SearchableDropdown from './components/SearchableDropdown';
 import HomepageLayout from './components/HomepageLayout';
-import Card from './components/Card';
+
+import Post, { ReactMarkdown } from './components/Post'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './images/DevScissors Graphics.jpg';
 
 
@@ -11,14 +13,23 @@ import logo from './images/DevScissors Graphics.jpg';
 function App() {
   return (
     <div className="App">
-      <HomepageLayout />
-      {/* <SearchableDropdown />
-      <Card />
-      <img src={logo} alt="DevScissors logo" />
-      <MarkdownExample />  */}
+      <Router>
+        <Switch>
+          <Route exact path='/' component={HomepageLayout}/>
+          <Route exact path='/post/:id' component={Post}/>
+     
+     {/* NOT FOUND HANDLER */}
+     {/* <Route component={NotFound} /> */}
 
+      </Switch>
+</Router>
     </div>
   );
 }
 
 export default App;
+
+  {/* <SearchableDropdown />
+      <Card />
+      <img src={logo} alt="DevScissors logo" />
+      <MarkdownExample />  */}
---
+++ src/components/Post.js
@@ -0,0 +1,402 @@
+import React, { Component } from 'react'
+
+import { Route, Link } from 'react-router-dom';
+import CardExampleCard from './CardExampleCard';
+
+import { createMedia } from '@artsy/fresnel'
+import PropTypes from 'prop-types'
+import SearchableDropdown from './SearchableDropdown';
+import './styles.css'
+import logo from '../images/DevScissors Graphics.jpg';
+
+/* eslint-disable max-classes-per-file */
+/* eslint-disable react/no-multi-comp */
+import {
+    Button,
+    Container,
+    Divider,
+    Grid,
+    Header,
+    Icon,
+    Image,
+    List,
+    Menu,
+    Segment,
+    Sidebar,
+    Visibility,
+} from 'semantic-ui-react'
+
+const { MediaContextProvider, Media } = createMedia({
+    breakpoints: {
+        mobile: 0,
+        tablet: 768,
+        computer: 1024,
+    },
+})
+
+
+
+
+/* Heads up!
+ * HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled
+ * components for such things.
+ */
+const HomepageHeading = ({ mobile }) => (
+    <Container text>
+        <div className="metaPost">
+            <CardExampleCard />
+            <CardExampleCard />
+            <CardExampleCard />
+            <CardExampleCard />
+        </div>
+
+        <Header
+            as='h1'
+            content='DevSc✂ssors'
+            inverted
+            style={{
+                fontSize: mobile ? '2em' : '4em',
+                fontWeight: 'normal',
+                marginBottom: 0,
+                marginTop: mobile ? '1.5em' : '3em',
+            }}
+        />
+        <SearchableDropdown />
+        <Header
+            as='h2'
+            content='Choose a topic for code snippets:'
+            inverted
+            style={{
+                fontSize: mobile ? '1.5em' : '1.7em',
+                fontWeight: 'normal',
+                marginTop: mobile ? '0.5em' : '1.5em',
+            }}
+        />
+        <Button primary size='huge' style={{
+            margin: '10px 10px 10px 10px',
+            backgroundColor: 'firebrick',
+            boxShadow: '3px 5px #080808',
+        }}>
+            HTML/CSS
+      <Icon name='right arrow' />
+        </Button>
+        <Button primary size='huge' style={{
+            margin: '10px 10px 10px 10px',
+            backgroundColor: 'rebeccapurple',
+            boxShadow: '3px 5px #080808'
+        }}>
+            JavaScript
+      <Icon name='right arrow' />
+        </Button>
+        <Button primary size='huge' style={{
+            margin: '10px 10px 10px 10px',
+            backgroundColor: 'green',
+            boxShadow: '3px 5px #080808'
+        }}>
+            React
+      <Icon name='right arrow' />
+        </Button>
+        <Button primary size='huge' style={{
+            margin: '10px 10px 10px 10px',
+            backgroundColor: 'grey',
+            boxShadow: '3px 5px #080808'
+        }}>
+            Node.js
+      <Icon name='right arrow' />
+        </Button>
+        <Button primary size='huge' style={{
+            margin: '10px 10px 10px 10px',
+            backgroundColor: 'orange',
+            boxShadow: '3px 5px #080808'
+        }}>
+            jQuery
+      <Icon name='right arrow' />
+        </Button>
+        <Button primary size='huge' style={{
+            margin: '10px 10px 10px 10px',
+            backgroundColor: 'firebrick',
+            boxShadow: '3px 5px #080808'
+        }}>
+            PostgreSQL
+      <Icon name='right arrow' />
+        </Button>
+    </Container>
+)
+
+HomepageHeading.propTypes = {
+    mobile: PropTypes.bool,
+}
+
+/* Heads up!
+ * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
+ * It can be more complicated, but you can create really flexible markup.
+ */
+class DesktopContainer extends Component {
+    state = {}
+
+    hideFixedMenu = () => this.setState({ fixed: false })
+    showFixedMenu = () => this.setState({ fixed: true })
+
+    render() {
+        const { children } = this.props
+        const { fixed } = this.state
+
+        return (
+            <Media greaterThan='mobile'>
+                <Visibility
+                    once={false}
+                    onBottomPassed={this.showFixedMenu}
+                    onBottomPassedReverse={this.hideFixedMenu}
+                >
+                    <Segment
+                        inverted
+                        textAlign='center'
+                        style={{ minHeight: 700, padding: '1em 0em' }}
+                        vertical
+                    >
+                        <Menu
+                            fixed={fixed ? 'top' : null}
+                            inverted={!fixed}
+                            pointing={!fixed}
+                            secondary={!fixed}
+                            size='large'
+                        >
+                            <Container>
+                                <Menu.Item as='a' active>
+                                    Home
+                </Menu.Item>
+                                <Menu.Item as='a'>Work</Menu.Item>
+                                <Menu.Item as='a'>Company</Menu.Item>
+                                <Menu.Item as='a'>Careers</Menu.Item>
+                                <Menu.Item position='right'>
+                                    <Button as='a' inverted={!fixed}>
+                                        Log in
+                  </Button>
+                                    <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
+                                        Sign Up
+                  </Button>
+                                </Menu.Item>
+                            </Container>
+                        </Menu>
+                        <HomepageHeading />
+                    </Segment>
+                </Visibility>
+
+                {children}
+            </Media>
+        )
+    }
+}
+
+DesktopContainer.propTypes = {
+    children: PropTypes.node,
+}
+
+class MobileContainer extends Component {
+    state = {}
+
+    handleSidebarHide = () => this.setState({ sidebarOpened: false })
+
+    handleToggle = () => this.setState({ sidebarOpened: true })
+
+    render() {
+        const { children } = this.props
+        const { sidebarOpened } = this.state
+
+        return (
+            <Media as={Sidebar.Pushable} at='mobile'>
+                <Sidebar.Pushable>
+                    <Sidebar
+                        as={Menu}
+                        animation='overlay'
+                        inverted
+                        onHide={this.handleSidebarHide}
+                        vertical
+                        visible={sidebarOpened}
+                    >
+                        <Menu.Item as='a' active>
+                            Home
+            </Menu.Item>
+                        <Menu.Item as='a'>Work</Menu.Item>
+                        <Menu.Item as='a'>Company</Menu.Item>
+                        <Menu.Item as='a'>Careers</Menu.Item>
+                        <Menu.Item as='a'>Log in</Menu.Item>
+                        <Menu.Item as='a'>Sign Up</Menu.Item>
+                    </Sidebar>
+
+                    <Sidebar.Pusher dimmed={sidebarOpened}>
+                        <Segment
+                            inverted
+                            textAlign='center'
+                            style={{ minHeight: 350, padding: '1em 0em' }}
+                            vertical
+                        >
+                            <Container>
+                                <Menu inverted pointing secondary size='large'>
+                                    <Menu.Item onClick={this.handleToggle}>
+                                        <Icon name='sidebar' />
+                                    </Menu.Item>
+                                    <Menu.Item position='right'>
+                                        <Button as='a' inverted>
+                                            Log in
+                    </Button>
+                                        <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
+                                            Sign Up
+                    </Button>
+                                    </Menu.Item>
+                                </Menu>
+                            </Container>
+                            <HomepageHeading mobile />
+                        </Segment>
+
+                        {children}
+                    </Sidebar.Pusher>
+                </Sidebar.Pushable>
+            </Media>
+        )
+    }
+}
+
+MobileContainer.propTypes = {
+    children: PropTypes.node,
+}
+
+const ResponsiveContainer = ({ children }) => (
+    /* Heads up!
+     * For large applications it may not be best option to put all page into these containers at
+     * they will be rendered twice for SSR.
+     */
+    <MediaContextProvider>
+        <DesktopContainer>{children}</DesktopContainer>
+        <MobileContainer>{children}</MobileContainer>
+    </MediaContextProvider>
+)
+
+ResponsiveContainer.propTypes = {
+    children: PropTypes.node,
+}
+
+const HomepageLayout = () => (
+    <ResponsiveContainer>
+        <Segment style={{ padding: '8em 0em' }} vertical>
+            <Grid container stackable verticalAlign='middle'>
+                <Grid.Row>
+                    <Grid.Column width={8}>
+                        <Header as='h3' style={{ fontSize: '2em' }}>
+                            This is a heading
+            </Header>
+                        <p style={{ fontSize: '1.33em' }}>
+                            This is a paragraph below the heading lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+            </p>
+                        <Header as='h3' style={{ fontSize: '2em' }}>
+                            This is another heading like the previous one
+            </Header>
+                        <p style={{ fontSize: '1.33em' }}>
+                            Oh yeah, and another paragraph right here. Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+            </p>
+                    </Grid.Column>
+                    <Grid.Column floated='right' width={6}>
+                        <Image src={logo} />
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column textAlign='center'>
+                        <Button size='huge'>Huge Button</Button>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </Segment>
+
+        <Segment style={{ padding: '0em' }} vertical>
+            <Grid celled='internally' columns='equal' stackable>
+                <Grid.Row textAlign='center'>
+                    <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+                        <Header as='h3' style={{ fontSize: '2em' }}>
+                            "What a Website"
+            </Header>
+                        <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
+                    </Grid.Column>
+                    <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+                        <Header as='h3' style={{ fontSize: '2em' }}>
+                            "Quote from someone."
+            </Header>
+                        <p style={{ fontSize: '1.33em' }}>
+                            <Image avatar src='/images/avatar/large/nan.jpg' />
+                            <b>Name</b> CEO at Somewhere Inc.
+            </p>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </Segment>
+
+        <Segment style={{ padding: '8em 0em' }} vertical>
+            <Container text>
+                <Header as='h3' style={{ fontSize: '2em' }}>
+                    Centered, Attention-Grabbing
+        </Header>
+                <p style={{ fontSize: '1.33em' }}>
+                    Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem IpsumLorem Ipsum Lorem Ipsum Lorem Ipsum
+        </p>
+                <Button as='a' size='large'>
+                    Read More
+        </Button>
+
+                <Divider
+                    as='h4'
+                    className='header'
+                    horizontal
+                    style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+                >
+                    <a href='#'>Another Section</a>
+                </Divider>
+
+                <Header as='h3' style={{ fontSize: '2em' }}>
+                    Is this really another header??
+        </Header>
+                <p style={{ fontSize: '1.33em' }}>
+                    Yes, it is. And this is another paragraph that can go on and on and on Lorem IpsumLorem IpsumLorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        </p>
+                <Button as='a' size='large'>
+                    I'm Still Quite Interested
+        </Button>
+            </Container>
+        </Segment>
+
+        <Segment inverted vertical style={{ padding: '5em 0em' }}>
+            <Container>
+                <Grid divided inverted stackable>
+                    <Grid.Row>
+                        <Grid.Column width={3}>
+                            <Header inverted as='h4' content='About' />
+                            <List link inverted>
+                                <List.Item as='a'>Sitemap</List.Item>
+                                <List.Item as='a'>Contact Us</List.Item>
+                                <List.Item as='a'>Something</List.Item>
+                                <List.Item as='a'>Something Else</List.Item>
+                            </List>
+                        </Grid.Column>
+                        <Grid.Column width={3}>
+                            <Header inverted as='h4' content='Services' />
+                            <List link inverted>
+                                <List.Item as='a'>Pre-Order</List.Item>
+                                <List.Item as='a'>FAQ</List.Item>
+                                <List.Item as='a'>How To Access</List.Item>
+                                <List.Item as='a'>Favorites</List.Item>
+                            </List>
+                        </Grid.Column>
+                        <Grid.Column width={7}>
+                            <Header as='h4' inverted>
+                                Footer Header
+              </Header>
+                            <p>
+                                Extra space for a call to action inside the footer that could help re-engage users.
+              </p>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Container>
+        </Segment>
+    </ResponsiveContainer>
+)
+
+export default HomepageLayout
